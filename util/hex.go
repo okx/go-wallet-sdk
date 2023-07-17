d5e43fdc66233a20f4a98c6d41fff4dc9df19da3
ares0x <jialijun1024@gmail.com>
2023-07-17 21:55:36 +0800

    fix: keep the logic of the RemoveZeroHex and DecodeHexString methods consistent.

--- util/hex.go
+++ util/hex.go
@@ -7,10 +7,8 @@ import (
 
 // delete the 0x from the front
 func RemoveZeroHex(s string) []byte {
-	if len(s) > 1 {
-		if s[0:2] == "0x" || s[0:2] == "0X" {
-			s = s[2:]
-		}
+	if strings.HasPrefix(s, "0x") || strings.HasPrefix(s, "0X") {
+		s = s[2:]
 	}
 	if len(s)%2 == 1 {
 		s = "0" + s

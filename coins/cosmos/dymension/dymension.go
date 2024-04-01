a571d7b038c81ccbcd7d1e5c78d976da1e78b89b
Halimao <1065621723@qq.com>
2024-04-01 19:00:09 +0800

    feat: add support for dymension

---
+++ coins/cosmos/dymension/dymension.go
@@ -0,0 +1,18 @@
+package dymension
+
+import (
+	"github.com/okx/go-wallet-sdk/coins/cosmos"
+)
+
+const (
+	HRP = "dym"
+)
+
+// NewAddress method of eth is used
+func NewAddress(privateKey string) (string, error) {
+	return cosmos.NewAddress(privateKey, HRP, true)
+}
+
+func ValidateAddress(address string) bool {
+	return cosmos.ValidateAddress(address, HRP)
+}
